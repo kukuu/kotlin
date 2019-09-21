c9f77728f0b7649245933b3377bd65efe625c1f9
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-09-21 14:24:29 +0100

    Deleted - made redundant. Moved.

--- snippets/defaultArguments.kt
+++
@@ -1,7 +0,0 @@
-
-//Default Arguments
-//No need to define several similar methods with varying arguments:
-
-fun build(title: String, width: Int = 800, height: Int = 600) {
-    Frame(title, width, height)
-}
