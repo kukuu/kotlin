afa2c940839284769e98afaa10e33fa8d43be2c5
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-09-21 14:31:05 +0100

    Deleted - made redundant. Moved.

--- snippets/typeInference.kt
+++
@@ -1,8 +0,0 @@
-
-//Kotlin will infer your types wherever you feel it will improve readability:
-
-val a = "abc"                         // type inferred to String
-val b = 4                             // type inferred to Int
-
-val c: Double = 0.7                   // type declared explicitly
-val d: List<String> = ArrayList()     // type declared explicitly
