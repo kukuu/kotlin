9e633ef17b57257d563e9d3b3082d7df3605fce0
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-09-21 14:24:59 +0100

    Deleted - made redundant. Moved.

--- snippets/intuitiveEquals.kt
+++
@@ -1,8 +0,0 @@
-//Intuitive Equals
-//You can stop calling equals() explicitly because the == operator now checks for structural equality:
-
-
-val john1 = Person("John")
-val john2 = Person("John")
-john1 == john2    // true  (structural equality)
-john1 === john2   // false (referential equality)
