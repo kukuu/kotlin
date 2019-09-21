e8acbc6e42206d0350f9cf4d463de5c424a7869a
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-09-21 14:26:01 +0100

    Deleted - made redundant. Moved.

--- snippets/stringInterpolation.kt
+++
@@ -1,5 +0,0 @@
-
-//It’s as if a smarter and more readable version of Java’s String.format() was built into the language:
-val x = 4
-val y = 7
-print("sum of $x and $y is ${x + y}")  // sum of 4 and 7 is 11
