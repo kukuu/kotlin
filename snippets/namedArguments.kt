ff2afd7ad170ed9034b5c1e43d672e0c6dbb6a0c
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-09-21 14:25:32 +0100

    Deleted - made redundant. Moved.

--- snippets/namedArguments.kt
+++
@@ -1,8 +0,0 @@
-
-
-//Named Arguments
-//Combined with default arguments, named arguments eliminates the need for builders:
-
-build("PacMan", 400, 300)                           // equivalent
-build(title = "PacMan", width = 400, height = 300)  // equivalent
-build(width = 400, height = 300, title = "PacMan")  // equivalent
