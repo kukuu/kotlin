97f7d754d31fbf9e8723a657777305764943dde0
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-09-23 11:52:32 +0100

    Deleted destructing from tld.

--- programs/destructurigDeclaration.kts
+++
@@ -1,8 +0,0 @@
-
-
-//Destructuring Declarations
-//Some objects can be destructured, which is for example useful for iterating maps:
-for ((key, value) in map) {
-    print("Key is: $key")
-    print("Value is: $value")
-}
