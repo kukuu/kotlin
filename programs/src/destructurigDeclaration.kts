75435ad1b29abd2d1b215e5bbff88f15a96d1e55
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-09-23 11:55:22 +0100

    moved destructuring declaration into src folder

---
+++ programs/src/destructurigDeclaration.kts
@@ -0,0 +1,8 @@
+
+
+//Destructuring Declarations
+//Some objects can be destructured, which is for example useful for iterating maps in loop:
+for ((key, value) in map) {
+    print("Key is: $key")
+    print("Value is: $value")
+}
