8aea63d30dc2ef457f09060e830b58dac9725ec7
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-09-30 05:39:10 +0100

    minor comment changes

--- programs/src/destructurigDeclaration.kts
+++ programs/src/destructurigDeclaration.kts
@@ -2,6 +2,7 @@
 
 //Destructuring Declarations
 //Some objects can be destructured, which is for example useful for iterating maps in loop:
+//Extracting key value pairs
 for ((key, value) in map) {
     print("Key is: $key")
     print("Value is: $value")
