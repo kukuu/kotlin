9ad39779c893c0ae374f971a8dd8afc64eeb3f2f
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2020-03-03 13:52:53 +0000

    Adding review notes to model

--- programs/src/data.kts
+++ programs/src/data.kts
@@ -1,8 +1,10 @@
 //The Data Class
 //It’s a POJO complete with toString(), equals(), hashCode(), and copy(), and unlike in Java it won’t take up 100 lines of code:
 
+//Model Class
 data class Person(val name: String,
                   var email: String,
                   var age: Int)
 
+//Create an instance - Object
 val john = Person("Jake", "jake@gmail.com", 112)
